a96047a3f091595b26dda8e53a4888ca709f1154
Nandhakumar A <fdemo1177@gmail.com>
2025-05-17 19:57:04 +0530

    push notification added

--- main.js
+++ main.js
@@ -1,1 +1,36 @@
 console.log("Main JS loaded.");
+
+
+// push notification 
+
+const button = document.getElementById("button");
+
+ button.addEventListener("click", () => {
+        Notification.requestPermission().then(permission => {
+            if (permission === "granted") {
+                console.log("The user accepted");
+                const notification = new Notification("Example notification", {
+                    body: Math.random(),
+                    data: {hello:"world"},
+                    icon:"/"
+                })
+
+                notification.addEventListener("error", e => {
+                    alert("error")
+                })
+            }
+        });
+    });
+
+ //push notification for user when they leave website or app.
+let notification
+document.addEventListener("visibilitychange", () => {
+    if (document.visibilityState === "hidden") {
+        notification = new Notification("come back please",{
+            body:"please",
+            tag:"come Back",
+        })
+    }else{
+        notification.close()
+    }
+})
